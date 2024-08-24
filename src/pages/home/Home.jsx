97238fcb5e044287d4be4dbe22b2db04149374dc
cnyt-y <56654010+cnyt-y@users.noreply.github.com>
2024-08-24 23:28:25 +0300

    düzen

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+    console.log("naber")
+  return (
+    <div>
+        <Navbar/>
+
+    </div>
+  )
+}
+export default Home
